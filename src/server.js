f3e5c960d2caaea9a46636cbdca188a6948bc732
amey09 <ameymhatre09@gmail.com>
2023-11-24 23:03:49 +0530

    refactor: optimize dietController.js
    Utilize env to access the PORT and REACTAPP_BASE_URL instead of hardcoding.
    This commit refactors 'getDietPlan.js'.

--- src/server.js
+++ src/server.js
@@ -1,23 +1,27 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import dietRoutes from "./routes/dietRoutes.js";
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: "*"
-}))
+app.use(
+  cors({
+    origin: process.env.REACTAPP_BASE_URL,
+  })
+);
 
-const port = 5000
+const PORT = process.env.PORT;
 
-app.use('/api/diet', dietRoutes)
+app.use("/api/diet", dietRoutes);
 
-app.get('/', (req, res) => {
-    res.send('API is running....');
-})
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
 
-app.listen(port, () => console.log(`Server started on ${port}`))
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+
+export default app;
